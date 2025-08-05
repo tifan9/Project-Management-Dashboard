c4ac042eae85fecb3d2ec689ec4cf2c49a8a7000
uwase sophie <uwasesophie91@gmail.com>
2025-08-05 16:26:56 +0200

    project management system

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold text-center p-5">typescript</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
